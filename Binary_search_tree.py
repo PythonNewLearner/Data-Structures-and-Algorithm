5e5f37898b1b85d50404c88524d5979a8da6e2b6
baich <baichen7788@gmail.com>
2019-05-30 07:07:20 +0800

    binary search tree insert Node

---
+++ Binary_search_tree.py
@@ -0,0 +1,30 @@
+class Node(object):
+    def __init__(self,data):
+        self.data=data
+        self.leftChild=None
+        self.rightChild=None
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root=None
+
+    def insert(self,data):
+        if not self.root:
+            self.root=Node(data)
+        else:
+            self,insertNode(data,self.root)
+    #O(logN)
+    def insertNode(self,data,node):
+        if data<node.data:
+            if node.leftChild:
+                self.insertNode(data,node.leftChild)
+            else:
+                node.leftChild=Node(data)
+
+        else:
+            if node.rightChild:
+                self.insertNode(data,node.rightChild)
+            else:
+                node.rightChild=Node(data)
+
+
