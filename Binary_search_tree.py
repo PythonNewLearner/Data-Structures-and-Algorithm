3faf1a8c6681e8dba88bafc89991ddbc408e68e2
baich <baichen7788@gmail.com>
2019-06-01 05:16:00 +0800

    traverse Max Min

--- Binary_search_tree.py
+++ Binary_search_tree.py
@@ -12,7 +12,7 @@ class BinarySearchTree(object):
         if not self.root:
             self.root=Node(data)
         else:
-            self,insertNode(data,self.root)
+            self.insertNode(data,self.root)
     #O(logN)  if the tree is balanced; if the tree is not balanced  --->  O(N)  , AVL RBT are needed
     def insertNode(self,data,node):
         if data<node.data:
@@ -27,4 +27,34 @@ class BinarySearchTree(object):
             else:
                 node.rightChild=Node(data)
 
+    def getMinValue(self):
+        if self.root:
+            return self.getMin(self.root)
 
+    def getMin(self,node):
+        if node.leftChild:
+            return self.getMin(node.leftChild)
+        return node.data
+
+    def getMaxValue(self):
+        if self.root:
+            return self.getMax(self.root)
+
+    def getMax(self,node):
+        if node.rightChild:
+            return self.getMax(node.rightChild)
+        return node.data
+
+    def traverse(self):
+        if self.root:
+            self.traverseInOrder(self.root)
+
+    def traverseInOrder(self,node):
+
+        if node.leftChild:
+            self.traverseInOrder((node.leftChild))
+
+        print('%s'%node.data)
+
+        if node.rightChild:
+            self.traverseInOrder(node.rightChild)
